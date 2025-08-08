c8bef7c5a107034ac8cd28e70ede4e0cfeecea18
s a <3254439010@qq.com.com>
2025-08-08 22:03:49 +0800

    change router settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Index from './pages/Index';
 import About from './pages/About';
 import ContactMe from './pages/ContactMe';
@@ -11,9 +10,9 @@ function App() {
     <header >
       <nav>
         <ul>
-          <li><a href="/">首页</a></li>
-          <li><a href="/about">关于我</a></li>      
-          <li><a href="/contact">作品</a></li>
+          <li><Link to="/">首页</Link></li>
+          <li><Link to="/">关于我</Link></li>  
+          <li><Link to="/">作品</Link></li>
         </ul>
       </nav>
     </header>
